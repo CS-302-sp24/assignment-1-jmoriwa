3632ceb2f0de0438f4bbf2ed96e571151f5867b2
jmoriwa <joe.moriwa@mnsu.edu>
2024-02-16 17:03:06 -0600

    finish assignment, passed tests

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,16 +1,31 @@
 public class DiningPhilosophers {
 
   public static void main(String[] args) throws InterruptedException {
-    Philosopher[] philosophers = new Philosopher[5];
-    Chopstick[] chopsticks = new Chopstick[5];
+    int num = Integer.parseInt(args[0]);
+    int cycles = Integer.parseInt(args[1]);
+    int maxThink = Integer.parseInt(args[2]);
+    int maxEat = Integer.parseInt(args[3]);
+
+    boolean difHand = Integer.parseInt(args[4]) == 1 ? true : false;
+
+    Philosopher[] philosophers = new Philosopher[num];
+    Chopstick[] chopsticks = new Chopstick[num];
     
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < num; ++i)
       chopsticks[i] = new Chopstick(i);
-    for (int i = 0; i < 5; ++i) {
-      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]);
+    
+    for (int i = 0; i < num; ++i) {
+      boolean rightHand;
+      if (difHand){
+        rightHand = i % 2 == 0 ? true : false;
+      }
+      else{rightHand = true;}
+      
+      
+      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % num], cycles, maxThink, maxEat, rightHand, i);
       philosophers[i].start();
     }
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < num; ++i)
       philosophers[i].join();
   }
 }
--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -3,25 +3,56 @@ import java.util.Random;
 class Philosopher extends Thread {
   private Chopstick left, right;
   private Random random;
-  private int thinkCount;
+  private int thinkCount = 0;
+  private int cycles;
+  private int maxThink;
+  private int maxEat;
+  private boolean rightHand;
+  private String id;
 
-  public Philosopher(Chopstick left, Chopstick right) {
+  public Philosopher(Chopstick left, Chopstick right, int cycles, int maxThink, int maxEat, boolean rightHanded, int id) {
     this.left = left; this.right = right;
+    this.cycles = cycles;
+    this.maxThink = maxThink;
+    this.maxEat = maxEat;
+    this.rightHand = rightHanded;
+    this.id = "Philosopher " + id;
     random = new Random();
   }
 
   public void run() {
     try {
-      while(true) {
-        ++thinkCount;
-        if (thinkCount % 10 == 0)
-          System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
-        synchronized(left) {                    // Grab left chopstick 
-          synchronized(right) {                 // Grab right chopstick 
-            Thread.sleep(random.nextInt(1000)); // Eat for a while
+      boolean forever = false;
+      if (cycles == 0) {forever = true;}
+      while(forever || cycles > 0) {
+        thinkCount += 1;
+        if (thinkCount % 10 == 0) {System.out.println(id + " has thought " + thinkCount + " times");}
+        int think = random.nextInt(maxThink);
+        Thread.sleep(think);     // Think for a while
+        System.out.println(id +" has thought for " + think + " units");
+        if (rightHand) {
+          synchronized(right) {
+            System.out.println(id + " wants right fork");
+            synchronized(left) {
+              System.out.println(id + " wants left fork");
+              int eat = random.nextInt(maxEat);
+              Thread.sleep(eat);
+              System.out.println(id + " has eaten for " + eat + " units");
+            }
           }
         }
+        else {
+          synchronized(left) {                    
+            System.out.println(id + " wants left fork");
+            synchronized(right) {     
+              System.out.println(id + " wants right fork");    
+              int eat = random.nextInt(maxEat);        
+              Thread.sleep(eat); // Eat for a while
+              System.out.println(id + " has eaten for " + eat + " units");
+          }
+        }
+      }
+      cycles -= 1;
       }
     } catch(InterruptedException e) {}
   }
